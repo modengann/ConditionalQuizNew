cc5d906b8f7c316fe48db72b9a44dc2d635c55a5
CaseyGann <cmoden@gannacademy.org>
2023-09-11 10:55:41 -0400

    init

---
+++ src/App.java
@@ -0,0 +1,20 @@
+/** Overall task: Take a number from the user and decide if it is a valid entry then decide if it is a leap year
+ *  
+ *  1. Ask the user for a year between 1000 and 3000 (including both 1000 and 3000)
+ *  2. If the user enters a value that is not between 1000 and 3000, respond with "Invalid input."
+ *  3. If the year was valid in step #2, check if it is a leap year (i.e. a year divisible by 4)
+ *      -If the year is a leap year (divisible by 4), print "The year is a leap year."
+ *      -If the year is not a leap year, print "The year is not a leap year."
+ *  4. Check if the year is divisible by 100.
+ *      -If the year is divisible by 100, print "The year is a century."
+ *      -If the year is not divisible by 100, print "The year is not a century."
+ */
+
+
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
